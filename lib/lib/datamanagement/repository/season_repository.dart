95eb4be0afc28ccaf2467099c2e9f4998ac5b0c0
Dalnoki Róbert <dalnoki.robert@webstar.hu>
2023-01-18 13:48:03 +0100

    Use ErrorHandler in SeasonRepository

--- lib/lib/datamanagement/repository/season_repository.dart
+++ lib/lib/datamanagement/repository/season_repository.dart
@@ -1,4 +1,6 @@
 import 'dart:convert';
+import 'dart:developer';
+import 'package:f1_application/lib/datamanagement/error_handler.dart';
 import 'package:f1_application/lib/model/season.dart';
 import 'package:f1_application/lib/network/api/ergast_api_client.dart';
 
@@ -7,13 +9,23 @@ class SeasonRepository {
 
   final ErgastApiClient clientErgast = ErgastApiClient();
 
+  final ErrorHandler _errorHandler = ErrorHandler();
+
   Future<List<Season>> fetchSeasons() async {
     List<Season> seasons = [];
-    final response = await clientErgast.get(type: GetType.season);
-    if (response.statusCode == 200) {
-      Map<String, dynamic> jsonObject = jsonDecode(response.body);
-      seasons = Season.listFromJson(
-          jsonObject["MRData"]["SeasonTable"]["Seasons"]);
+    try {
+      final response = await clientErgast.get(type: GetType.season);
+      switch (response.statusCode) {
+        case 200:
+          Map<String, dynamic> jsonObject = jsonDecode(response.body);
+          seasons = Season.listFromJson(
+              jsonObject["MRData"]["SeasonTable"]["Seasons"]);
+          break;
+        default:
+          throw _errorHandler.getErrorResponse(response);
+      }
+    } catch (e) {
+      log(e.toString());
     }
     return seasons;
   }
