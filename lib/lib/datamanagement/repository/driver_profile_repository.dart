0f41dbf96e47c5b4bfd1bc5eda50e9fefcf4f25c
Dalnoki Róbert <dalnoki.robert@webstar.hu>
2023-01-18 13:47:43 +0100

    Use ErrorHandler in DriverProfileRepository

--- lib/lib/datamanagement/repository/driver_profile_repository.dart
+++ lib/lib/datamanagement/repository/driver_profile_repository.dart
@@ -1,4 +1,6 @@
 import 'dart:convert';
+import 'dart:developer';
+import 'package:f1_application/lib/datamanagement/error_handler.dart';
 import 'package:f1_application/lib/network/api/google_search_api_client.dart';
 
 class DriverProfileRepository {
@@ -6,12 +8,22 @@ class DriverProfileRepository {
 
   final GoogleSearchApiClient clientGoogleSearch = GoogleSearchApiClient();
 
+  final ErrorHandler _errorHandler = ErrorHandler();
+
   Future<String> fetchGoogleImageUrl(String driverLastName) async {
     String url = "";
-    final response = await clientGoogleSearch.get(queryString: driverLastName);
-    if (response.statusCode == 200) {
-      Map<String, dynamic> jsonObject = jsonDecode(response.body);
-      url = jsonObject["items"][0]["pagemap"]["cse_image"][0]["src"] ?? "";
+    try {
+      final response = await clientGoogleSearch.get(queryString: driverLastName);
+      switch (response.statusCode) {
+        case 200:
+          Map<String, dynamic> jsonObject = jsonDecode(response.body);
+          url = jsonObject["items"][0]["pagemap"]["cse_image"][0]["src"] ?? "";
+          break;
+        default:
+          throw _errorHandler.getErrorResponse(response);
+      }
+    } catch (e) {
+      log(e.toString());
     }
     return url;
   }
