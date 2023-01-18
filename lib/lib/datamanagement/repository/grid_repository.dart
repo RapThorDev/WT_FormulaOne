fefeca9274f861f6cc4b410716a7c0f87cf57337
Dalnoki Róbert <dalnoki.robert@webstar.hu>
2023-01-18 13:47:54 +0100

    Use ErrorHandler in GridRepository

--- lib/lib/datamanagement/repository/grid_repository.dart
+++ lib/lib/datamanagement/repository/grid_repository.dart
@@ -1,19 +1,33 @@
 import 'dart:convert';
+import 'dart:developer';
 import 'package:f1_application/lib/model/driver.dart';
 import 'package:f1_application/lib/network/api/ergast_api_client.dart';
+import 'package:http/http.dart';
+import 'package:f1_application/lib/datamanagement/error_handler.dart';
 
 class GridRepository {
   GridRepository();
 
   final ErgastApiClient clientErgast = ErgastApiClient();
 
+  final ErrorHandler _errorHandler = ErrorHandler();
+
   Future<List<Driver>> fetchGrid(int  seasonYear) async {
     List<Driver> drivers = [];
-    final response = await clientErgast.get(type: GetType.grid, year: seasonYear);
-    if (response.statusCode == 200) {
-      Map<String, dynamic> jsonObject = jsonDecode(response.body);
-      drivers = Driver.listFromJson(jsonObject["MRData"]["DriverTable"]["Drivers"]);
+    try {
+      Response response = await clientErgast.get(type: GetType.grid, year: seasonYear);
+      switch (response.statusCode) {
+        case 200:
+          Map<String, dynamic> jsonObject = jsonDecode(response.body);
+          drivers = Driver.listFromJson(jsonObject["MRData"]["DriverTable"]["Drivers"]);
+          break;
+        default:
+          throw _errorHandler.getErrorResponse(response);
+      }
+    } catch (e) {
+      log(e.toString());
     }
+
     return drivers;
   }
 }
